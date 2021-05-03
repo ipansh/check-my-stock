bb9bb1a1ae2a091258788a1aee6c10506bfdd6f8
Ilya <ilya.panshenskov@italki.com>
2021-05-03 15:54:25 +0800

    adding one more class

--- CheckMyStockVitals.py
+++ CheckMyStockVitals.py
@@ -5,7 +5,7 @@ class CheckMyStockVitals:
     def __init__(self):
 
         """ Base class with a couple of functions that return basic info.
-            """
+        """
 
     def check_vitals(company_name):
         stock_ticker = yf.Ticker(company_name)
@@ -14,4 +14,11 @@ class CheckMyStockVitals:
         name = stock_info['shortName']
         print("The Market Open Price today for {} is ${}.".format(name, price))
 
-###sanitychecker
+class VisualizeMyStock(CheckMyStockVitals):
+    """ Binomial distribution class for calculating and
+    visualizing a Binomial distribution.
+    """
+
+    def __init__(self):
+
+        CheckMyStockVitals.__init__(self)
