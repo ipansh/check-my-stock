42bfbe77cb389b17345e62bb035250679de3b85d
Ilya <ilya.panshenskov@italki.com>
2021-12-11 13:26:52 +0800

    adding api key

--- base.py
+++ base.py
@@ -3,14 +3,7 @@ import requests
 from datetime import datetime
 import os
 
-#_KEY_ = os.environ['rapidapi_key']
-
-
-with open('/Users/ilya/Desktop/keys/yahoo_finance.txt','r') as file:
-    key = file.readlines()
-
-_KEY_ = key[0]
-
+_KEY_ = os.environ['rapidapi_key']
 
 def get_json_response(ticker, url):
 
