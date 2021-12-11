3b7d8e69f766313c0e246b2c5f9db73348ffecf3
Ilya <ilya.panshenskov@italki.com>
2021-12-11 11:53:32 +0800

    making html static

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def home():
                   xaxis_title = 'Date',
                   height = 600, width = 1200)
 
-    fig.write_html('templates/plotly_page.html', full_html=False, include_plotlyjs='cdn')
+    #fig.write_html('templates/plotly_page.html', full_html=False, include_plotlyjs='cdn')
 
     return render_template('plotly_page.html') 
 
