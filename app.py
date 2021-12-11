15e7e5008e65e9a896a91b834ec46c094638d9e0
Ilya <ilya.panshenskov@italki.com>
2021-12-11 15:37:22 +0800

    adding styles

--- app.py
+++ app.py
@@ -8,10 +8,6 @@ import numpy as np
 
 app = Flask(__name__)
 
-#@app.route("/")
-#def home():
-#    return 'Hello!'
-
 def get_stock_data():
     main_daily_df = base.get_stock_daily_price('ABBV',30)
     my_tickers = ['EXC','BABA','JD','AMAT']
@@ -26,29 +22,30 @@ def create_plot(main_daily_df):
 
     fig = go.Figure()
 
-    fig.add_trace(go.Scatter(x=main_daily_df['date'], y=main_daily_df['abbv'], name='abbv_%', mode='lines+markers'))
-    fig.add_trace(go.Scatter(x=main_daily_df['date'], y=main_daily_df['exc'], name='exc_%', mode='lines+markers'))
-    fig.add_trace(go.Scatter(x=main_daily_df['date'], y=main_daily_df['baba'], name='baba_%', mode='lines+markers'))
-    fig.add_trace(go.Scatter(x=main_daily_df['date'], y=main_daily_df['jd'], name='jd_%', mode='lines+markers'))
-    fig.add_trace(go.Scatter(x=main_daily_df['date'], y=main_daily_df['amat'], name='amat_%', mode='lines+markers'))
+    fig.add_trace(go.Scatter(x=main_daily_df['date'], y=main_daily_df['abbv'], name='Abbvie', mode='lines+markers'))
+    fig.add_trace(go.Scatter(x=main_daily_df['date'], y=main_daily_df['exc'], name='Exelon', mode='lines+markers'))
+    fig.add_trace(go.Scatter(x=main_daily_df['date'], y=main_daily_df['baba'], name='Alibaba', mode='lines+markers'))
+    fig.add_trace(go.Scatter(x=main_daily_df['date'], y=main_daily_df['jd'], name='Jindong', mode='lines+markers'))
+    fig.add_trace(go.Scatter(x=main_daily_df['date'], y=main_daily_df['amat'], name='Applied Materials', mode='lines+markers'))
 
     fig.update_layout(title_text = 'Daily Stock Price Changes',
                   yaxis_title = 'Stock Price',
                   xaxis_title = 'Date',
-                  height = 600, width = 1200)
+                  height = 600, width = 1200,
+                  title_font_family="Gill Sans")
 
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return graphJSON
 
-@app.route("/")
+@app.route("/stock_prices")
 def home():
     
     main_daily_df = get_stock_data()
 
     bar = create_plot(main_daily_df)
 
-    return render_template('plotly_page.html', plot=bar) 
+    return render_template('stock_prices.html', plot=bar) 
 
 if __name__  == '__main__': 
     app.run(debug=True)
